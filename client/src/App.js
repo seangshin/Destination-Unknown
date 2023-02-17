9138060684a68374edc08e09a692e2208d0676ba
“kara <bouncingpie@icloud.com>
2023-02-16 16:57:48 -0800

    added the react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import React from "react";
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -33,8 +38,13 @@ function App() {
   return (
     <div>
       <ApolloProvider client={client}>
-        {/* <Signup /> */}
-        <Login /> 
+        <BrowserRouter>
+          <Routes>
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+          <Login />
+        </BrowserRouter>
       </ApolloProvider>
     </div>
   );
