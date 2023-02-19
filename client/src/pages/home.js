926334256ae7b8e8474ca2539aafbaadd135a02e
CinYP <111353883+CinYP@users.noreply.github.com>
2023-02-18 20:02:02 -0500

    Merge branch 'main' of github.com:seangshin/Destination-Unknown into CPBranch

---
+++ client/src/pages/home.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// import React from 'react'
+// import "../styles/Home.css"
+
+// export default function Home() {
+//   return (
+//     <div className='home'>
+//       <div className='about'>
+//           <h2>Hi, My Name is Cinthia Pruitt </h2>
+//           <div className='aboutinformation'>
+//               <p>
+//                 Highly motivated Full-Stack Software Engineer and Marketer with experience in Digital Marketing Strategy Consulting, Sales & Marketing, and Software Development. I have a strong background in developing and executing comprehensive Social Media strategies, Search Engine Optimization, Software Development, and design for Enterprise Clients.
+//               </p>
+//               <div className='icons'>
+//               - Linkedin Github Email Icons coming soon -
+//               </div>
+//           </div>
+//       </div>
+//       <div className='skills'>
+//           <h2>
+//             Skills
+//           </h2>
+//           <ol className='skillslist'>
+//             <li className='skillslist-item'> 
+//             <h3>Front End </h3>
+//             <span> HTML5, CSS3, BootStrap, React, Webpack </span>
+//             </li>
+//             <li className='skillslist-item'> 
+//             <h3>Back-End </h3>
+//             <span> JavaScript, jQuery, Express.js, Node.js, Progressive Web Apps, MySQL, MangoDB, ApolloServer </span>
+//             </li>
+//             <li className='skillslist-item'> 
+//             <h3> Languages </h3>
+//             <span> HTML, CSS, JavaScript</span>
+//             </li>
+//             <li className='skillslist-item'> 
+//             <h3> Marketing </h3>
+//             <span> Paid Social Media Management, Paid Search, Organic Social Media Management, Search Engine Optimization </span>
+//             </li>
+//           </ol>
+//       </div>
+
+//     </div>
+//   )
+// }
+
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,9 @@
+//const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 // make a search to google places api
 // https://maps.googleapis.com/maps/api/place/textsearch/json?query=Atlanta&key=AIzaSyDEHGBibTeuDpUclYDLNXIAZ0J7NKWewJw
-export const searchGoogleAPI = (query) => {
+export const searchGooglePlaces = (query) => {
+  //console.log(proxyurl+`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=AIzaSyDEHGBibTeuDpUclYDLNXIAZ0J7NKWewJw`);
+  //return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=AIzaSyDEHGBibTeuDpUclYDLNXIAZ0J7NKWewJw`);
 };
