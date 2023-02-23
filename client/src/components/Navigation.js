159500206f002254f8c242cbc89bb595f2b00e1b
Sean Shin <shin.seang@gmail.com>
2023-02-23 01:11:58 -0500

    updated to add Link in navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import "../styles/navbar.css";
@@ -8,7 +9,7 @@ export default function Navigation() {
   return (
     <>
     <Navbar className="nav-container" bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand  href='/'>
+        <Navbar.Brand as={Link} to='/'>
           <img
             alt=""
             src="./logo.png"
@@ -23,13 +24,13 @@ export default function Navigation() {
           <Nav className="mr-auto">
             {Auth.loggedIn() ? (
               <>
-                <Nav.Link href='/profile'>Profile</Nav.Link>
+                <Nav.Link as={Link} to='/profile'>Profile</Nav.Link>
                 <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
               </>
             ) : (
               <>
-                <Nav.Link href='/login' className="d-inline-block center">Login</Nav.Link>
-                <Nav.Link href='/signup'>Sign Up!</Nav.Link>
+                <Nav.Link as={Link} to='/login' className="d-inline-block center">Login</Nav.Link>
+                <Nav.Link as={Link} to='/signup'>Sign Up!</Nav.Link>
               </>
             )}
           </Nav>
