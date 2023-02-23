307ab02ce323a4a767f2a1e439790e80900c3869
“kara <bouncingpie@icloud.com>
2023-02-23 00:02:26 -0500

    styling and profiling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Jumbotron, Container, Button, Card, CardColumns } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_SEARCH } from '../utils/mutations';
+import React, { useState, useEffect } from "react";
+import "../styles/profile.css";
+import {
+  Jumbotron,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
+
+import { useQuery, useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_SEARCH } from "../utils/mutations";
 
 const Profile = () => {
   const { loading, data } = useQuery(GET_ME);
@@ -35,8 +45,8 @@ const Profile = () => {
 
     try {
       // // Execute the REMOVE_SEARCH mutation using the removeSearch data
-      const { data } = await removeSearch({ 
-        variables:  { restaurantId },
+      const { data } = await removeSearch({
+        variables: { restaurantId },
       });
     } catch (err) {
       console.error(err);
@@ -45,27 +55,36 @@ const Profile = () => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved searches!</h1>
         </Container>
       </Jumbotron>
       <Container>
-        <h2>
+        <h2 className="profileH2">
           {userData.savedLocations.length
-            ? `Viewing ${userData.savedLocations.length} saved ${userData.savedLocations.length === 1 ? 'searches' : 'searches'}:`
-            : 'You have no saved searches!'}
+            ? `Viewing ${userData.savedLocations.length} saved ${
+                userData.savedLocations.length === 1 ? "searches" : "searches"
+              }:`
+            : "You have no saved searches!"}
         </h2>
         <CardColumns>
           {userData.savedLocations.map((restaurant) => {
             return (
-              <Card key={restaurant.restaurantId} border='dark'>
-                <Card.Img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${restaurant.photo}&key=AIzaSyDEHGBibTeuDpUclYDLNXIAZ0J7NKWewJw`} alt={`The image for ${restaurant.restaurantName}`} variant='top' />
+              <Card key={restaurant.restaurantId} border="dark">
+                <Card.Img
+                  src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${restaurant.photo}&key=AIzaSyDEHGBibTeuDpUclYDLNXIAZ0J7NKWewJw`}
+                  alt={`The image for ${restaurant.restaurantName}`}
+                  variant="top"
+                />
                 <Card.Body>
                   <Card.Title>{restaurant.restaurantName}</Card.Title>
                   <Card.Subtitle>{`Restaurant Price: ${restaurant.priceLevel} and Rating: ${restaurant.rating}`}</Card.Subtitle>
                   {/* <Card.Text>{restaurant.description}</Card.Text> */}
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteSearch(restaurant.restaurantId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteSearch(restaurant.restaurantId)}
+                  >
                     Delete
                   </Button>
                 </Card.Body>
@@ -76,9 +95,6 @@ const Profile = () => {
       </Container>
     </>
   );
-}
-
-  
-
+};
 
 export default Profile;
